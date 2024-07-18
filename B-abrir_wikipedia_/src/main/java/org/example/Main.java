4261f1a8af44d33aeb488b806e5f49429d8d788b
PaulaNuness <127285238+PaulaNuness@users.noreply.github.com>
2024-07-18 19:22:23 +0200

    18-07-2024__19:22

--- B-abrir_wikipedia_/src/main/java/org/example/Main.java
+++ B-abrir_wikipedia_/src/main/java/org/example/Main.java
@@ -1,15 +1,19 @@
 package org.example;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) {
-
+        WebElement busqueda_google;
+        WebElement link_wikipedia;
 
         // Instanciar el WebDriver utilizando Chrome
         WebDriver driver = new ChromeDriver();
+
         /***************************************************/
         try{
             // Maximizar a janela do navegador
@@ -43,15 +47,49 @@ public class Main {
             Thread.sleep(3000);
 
             /***************************************************/
-            //escreber en la busqueda wikipedia y pulsar enter
-            driver.findElement(By.name("q")).sendKeys("Wikipedia");
+
+            //escreber en la busqueda wikipedia
+            busqueda_google = driver.findElement(By.name("q"));
+            busqueda_google.sendKeys("Wikipedia");
+            //driver.findElement(By.name("q")).sendKeys("Wikipedia"); tambien puedo hacer así
+
             /***************************************************/
 
             // quiero esperar 3 segundos para ver mejor
             Thread.sleep(3000);
 
             /***************************************************/
-            
+
+            //Pulsar Enter
+            busqueda_google.sendKeys(Keys.RETURN);
+            //busqueda_google.sendKeys(Keys.ENTER);tambien puedo hacer así
+
+            /***************************************************/
+
+            // quiero esperar 3 segundos para ver mejor
+            Thread.sleep(3000);
+
+            /***************************************************/
+
+            //Escribir por pantalla el titulo de la pagina
+            System.out.println("Page title is: " + driver.getTitle());
+
+            /***************************************************/
+
+            //pulsar no primero no enlace para entrar en wikipedia
+            // Encontrar y pulsar el enlace "Wikipedia"
+            link_wikipedia = driver.findElement(By.cssSelector("a[href='https://es.wikipedia.org/wiki/Wikipedia:Portada']"));
+            link_wikipedia.click();
+
+            /***************************************************/
+
+            // quiero esperar 3 segundos para ver mejor
+            Thread.sleep(3000);
+
+            /***************************************************/
+
+            //Escribir por pantalla el titulo de la pagina
+            System.out.println("Page title is: " + driver.getTitle());
 
             /***************************************************/
         }catch (Exception e){
